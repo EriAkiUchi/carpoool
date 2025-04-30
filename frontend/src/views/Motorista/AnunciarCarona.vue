d7d4d32f2d45aaee26c8966b5167e9df0d1d240f
Eric <eric@grafcet.com.br>
2025-04-30 15:40:05 -0300

    :sparkles: feat(AnunciarCarona): adicionar botão para voltar para a Dashboard

--- frontend/src/views/Motorista/AnunciarCarona.vue
+++ frontend/src/views/Motorista/AnunciarCarona.vue
@@ -12,7 +12,37 @@ const motoristaId = ref(authStore.user?.id);
 
 <template>
     <h2>Criar anúncio</h2>
+
+    <div class="back-link">
+      <router-link to="/motorista" class="btn-voltar">Voltar ao Dashboard</router-link>
+    </div>
 </template>
 
 <style scoped>
+
+.btn-voltar, .btn-primary {
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 0.9rem;
+  font-weight: 500;
+  transition: background-color 0.3s;
+  text-decoration: none;
+  display: inline-block;
+}
+.btn-voltar, .btn-primary {
+  background-color: #336699;
+  color: white;
+  margin-top: 2rem;
+}
+
+.btn-voltar:hover, .btn-primary:hover {
+  background-color: #264d73;
+}
+
+.back-link {
+  text-align: center;
+  margin-top: 2rem;
+}
 </style>
