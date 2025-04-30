19a2301b869013f447857dcf6e350075120abc8b
Eric <eric@grafcet.com.br>
2025-04-30 15:25:06 -0300

    :truck: dependency(AnunciarCarona): adicionar alguns imports

--- frontend/src/views/Motorista/AnunciarCarona.vue
+++ frontend/src/views/Motorista/AnunciarCarona.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { userAuthStore } from '@/store/auth';
 
+const authStore = userAuthStore();
+const router = useRouter();
+const user = ref(authStore.user);
+
+const motoristaId = ref(authStore.user?.id);
 </script>
 
 <template>
