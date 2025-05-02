0863dd03522a5e0fc88fc90a59ca361d2ca68a91
Eric <eric@grafcet.com.br>
2025-05-02 09:28:10 -0300

    :sparkles: feat(Solicitacoes): criar página de solicitações de carona

---
+++ frontend/src/views/Motorista/Solicitacoes.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { userAuthStore } from '@/store/auth';
+import viagemService from '@/services/viagemService';
+import type Viagem from '@/interfaces/IViagem';
+import passageiroService from '@/services/passageiroService';
+
+const authStore = userAuthStore();
+const route = useRoute();
+const user = ref(authStore.user);
+const viagens = ref<Viagem[]>([]);
+const isLoading = ref(true);
+const error = ref<string | null>(null);
+
+</script>
+
+<template>
+<section class="solicitacoes_container">
+
+</section>
+</template>
+
+<style scoped></style>
