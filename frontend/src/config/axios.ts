73fb552d0fefc68d30208a8db6f18b50e3b69d07
Eric <ericuchiyamada@gmail.com>
2025-05-16 20:21:07 -0300

    :recycle: refactor: criar variável de ambiente

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //criando uma instância do axios com a URL base
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_API_URL, // URL base da API
     headers: {
         'Content-Type': 'application/json',
     },
